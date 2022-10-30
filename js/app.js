8da10eda0c8c94f6e02d82763de22e376bddbc02
majharul <majharuljony163@gmail.com>
2022-10-30 17:58:48 +0600

    design multiple section

--- js/app.js
+++ js/app.js
@@ -8,3 +8,26 @@ $(".nav-link").click(function () {
   $(".nav-link").removeClass("active");
   $(this).addClass("active");
 });
+
+$(".do-slider").slick({
+  infinite: true,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  fadeSpeed: 1000,
+  dotsClass: "do-dots",
+});
+
+$(".work-slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  dotsClass: "work-dots",
+});
